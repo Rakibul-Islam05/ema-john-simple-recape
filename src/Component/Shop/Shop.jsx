fedc732ecf204604994d0b6a9a3ac96cbda0985f
Rakibul Islam <rakibulrakib05@gmail.com>
2024-02-07 01:23:49 +0600

    show data form cart

--- src/Component/Shop/Shop.jsx
+++ src/Component/Shop/Shop.jsx
@@ -6,7 +6,7 @@ import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
-    const [cart,setCart] = useState([])
+    const [cart, setCart] = useState([])
 
     useEffect(() => {
         fetch('products.json')
@@ -14,14 +14,45 @@ const Shop = () => {
             .then(data => setProducts(data))
     }, [])
 
-    useEffect(()=>{
+    useEffect(() => {
         const storedCart = getShoppingCart();
-        console.log(storedCart);
-    },[])
+        const savedCart = [];
 
-    const handleAddToCart  = (product) =>{
+        //get id of the addedProduct
+        for (const id in storedCart) {
+
+            //get product from products state by using id
+            const addedProduct = products.find(product => product.id === id)
+
+            if (addedProduct) {
+
+                //add quantity
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+
+                //add the add product to the saved cart
+                savedCart.push(addedProduct)
+            }
+        }
+
+        //set the cart
+        setCart(savedCart)
+    }, [products])
+
+    const handleAddToCart = (product) => {
         // console.log(product)
-        const newCart = [...cart,product];
+        let newCart = [];
+        const exists = cart.find(pd => pd.id == product.id)
+        if(!exists){
+            product.quantity=1;
+            newCart = [...cart, product];
+        }
+        else{
+            exists.quantity = exists.quantity + 1;
+            const remaining = cart.filter(pd => pd.id !== product.id)
+            newCart = [...remaining,exists]
+        }
+
         setCart(newCart);
         addToDb(product.id)
     }
