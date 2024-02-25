fa724f921847ae52acecd930f7b9432e66dec368
Rakibul Islam <rakibulrakib05@gmail.com>
2024-02-26 05:41:26 +0600

    implement delete from cart

--- src/Component/ReviewItem/ReviewItem.jsx
+++ src/Component/ReviewItem/ReviewItem.jsx
@@ -3,7 +3,7 @@ import './reviewItem.css'
 import { MdDelete } from "react-icons/md";
 
 
-const ReviewItem = ({ product }) => {
+const ReviewItem = ({ product,handleRemoveFromCart }) => {
     const { id, img, price, name, quantity } = product;
     return (
         <div className='review-item'>
@@ -13,8 +13,8 @@ const ReviewItem = ({ product }) => {
                 <p>Price: <span className='orange-text'>${price}</span></p>
                 <p>Order Quantity: <span className='orange-text'>${quantity}</span></p>
             </div>
-            
-            <button className='btn-style'>
+
+            <button onClick={() => handleRemoveFromCart(id)} className='btn-style'>
                 <MdDelete className='delete' />
             </button>
         </div>
